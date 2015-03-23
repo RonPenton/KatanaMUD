47dffffbafd33205054fc65fd49c22fddc0e2588
RonPenton <ron.penton@gmail.com>
2015-03-23 08:17:43 -0400

    Database Script

---
+++ src/KatanaMUD/Models/Database.sql
@@ -0,0 +1,19 @@
+﻿USE [KatanaMUD]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 03/23/2015 08:16:46 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[User](
+	[Id] [nvarchar](50) NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
+	[LockoutEnd] [datetimeoffset](7) NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[IsConfirmed] [bit] NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
