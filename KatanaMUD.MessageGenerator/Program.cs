89ba3e569823a27c0990cee536bf233783be4cd3
RonPenton <ron.penton@gmail.com>
2015-03-24 10:50:42 -0400

    Message Refactoring and Typescript Gen Tool started

---
+++ KatanaMUD.MessageGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaMUD.MessageGenerator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var dll = Assembly.ReflectionOnlyLoadFrom(@"..\..\..\artifacts\bin\KatanaMUD.Messages\Debug\aspnet50\KatanaMUD.Messages.dll");
+
+			var messageType = dll.GetTypes().Single(x => x.Name == "MessageBase");
+			var messages = dll.GetTypes().Where(x => x.IsSubclassOf(messageType));
+
+		}
+	}
+}
