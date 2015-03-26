0660085b32e887e4bbcc8cf2fe22545c6588947c
RonPenton <ron.penton@gmail.com>
2015-03-26 09:07:18 -0400

    Database Schema Update

--- src/KatanaMUD/Models/Database.sql
+++ src/KatanaMUD/Models/Database.sql
@@ -1,6 +1,20 @@
 ﻿USE [KatanaMUD]
 GO
-/****** Object:  Table [dbo].[User]    Script Date: 03/23/2015 08:16:46 ******/
+/****** Object:  Table [dbo].[WeaponType]    Script Date: 03/26/2015 09:06:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[WeaponType](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_WeaponType] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[User]    Script Date: 03/26/2015 09:06:00 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -12,6 +26,66 @@ CREATE TABLE [dbo].[User](
 	[PasswordHash] [nvarchar](max) NULL,
 	[IsConfirmed] [bit] NOT NULL,
  CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ClassTemplateWeaponType]    Script Date: 03/26/2015 09:06:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ClassTemplateWeaponType](
+	[ClassTemplateId] [int] NOT NULL,
+	[WeaponTypeId] [int] NOT NULL,
+ CONSTRAINT [PK_ClassTemplateWeaponType] PRIMARY KEY CLUSTERED 
+(
+	[ClassTemplateId] ASC,
+	[WeaponTypeId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ClassTemplateArmorType]    Script Date: 03/26/2015 09:06:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ClassTemplateArmorType](
+	[ClassTemplateId] [int] NOT NULL,
+	[ArmorTypeId] [int] NOT NULL,
+ CONSTRAINT [PK_ClassTemplateArmorType] PRIMARY KEY CLUSTERED 
+(
+	[ClassTemplateId] ASC,
+	[ArmorTypeId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ClassTemplate]    Script Date: 03/26/2015 09:06:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ClassTemplate](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[HpMin] [int] NOT NULL,
+	[HpMax] [int] NOT NULL,
+ CONSTRAINT [PK_ClassTemplate] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ArmorType]    Script Date: 03/26/2015 09:06:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ArmorType](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_ArmorType] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
